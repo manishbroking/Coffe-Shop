839723d468fd68f2435d46f79d39defb32ce0c50
Manish Gupta <manishbrokingofficial@gmail.com>
2024-12-19 12:00:58 +0530

    .

---
+++ setapt.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 8080;
+const path = require('path')
+app.use(express.static('public'))
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  res.sendFile("index.html")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
